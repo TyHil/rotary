da313b545ae5c704d97db297adb07a4175b27260
Tyler Hill <gotyhill@gmail.com>
2024-01-23 13:40:35 -0600

    Bedside lamp fix

--- rotary.py
+++ rotary.py
@@ -214,12 +214,12 @@ async def alarm():
     day = date.today().weekday()
     if day == 0 or day == 1 or day == 2 or day == 3:
         global alarmOn, alarmSkip
-        global ledStripOn, bedsideLampOn
+        #global ledStripOn, bedsideLampOn
         if alarmOn and not(alarmSkip) and ledStripOn is not None:
             await ledStripOn.command('main', 'switch', 'on')
             await asyncio.sleep(10)
             sendToArduino(0, 17, 5)
-            for brightness in range(17*2, 17*6+1, 17):
+            for brightness in range(17*2, 17*7+1, 17):
                 await asyncio.sleep(60*5) # 2
                 sendToArduino(0, brightness, 5)
             await bedsideLampOn.command('main', 'switch', 'on')
